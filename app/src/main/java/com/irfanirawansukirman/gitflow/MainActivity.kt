fa8825b8951ed380425fdb590595855075af6e8e
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2020-11-06 12:36:29 +0700

    add log

--- app/src/main/java/com/irfanirawansukirman/gitflow/MainActivity.kt
+++ app/src/main/java/com/irfanirawansukirman/gitflow/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("a", "bcde")
         Log.d("a", "bcde")
+        Log.d("a", "bcde")
     }
 }
